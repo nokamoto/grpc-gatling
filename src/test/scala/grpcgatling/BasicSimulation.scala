a53e4a7db0831a18ab22cb7d39ed8a36bc1e6906
Naofumi Okamoto <nokamoto.engr@gmail.com>
2019-02-14 09:17:20 +0900

    Handle error status (#3)

--- src/test/scala/grpcgatling/BasicSimulation.scala
+++ src/test/scala/grpcgatling/BasicSimulation.scala
@@ -1,43 +1,15 @@
 package grpcgatling
 
-import grpcgatling.grpc.{GrpcActionBuilder, GrpcProtocol}
-import grpcgatling.server.PingServer
+import grpcgatling.grpc.GrpcActionBuilder
 import io.gatling.commons.validation._
 import io.gatling.core.Predef._
 import io.gatling.core.session.Expression
-import ping.Message
 
-import scala.concurrent.ExecutionContext
-import scala.concurrent.duration._
 import scala.util.Try
 
-class BasicSimulation extends Simulation {
-  private[this] val port = 9001
-
-  private[this] val server = PingServer.server(port, ExecutionContext.global)
-
-  private[this] val protocol: GrpcProtocol = GrpcProtocol("localhost", 9001)
-
-  private[this] def grpc(name: Expression[String])(
+abstract class BasicSimulation extends Simulation {
+  protected[this] def grpc(name: Expression[String])(
       f: Session => Unit): GrpcActionBuilder = {
     GrpcActionBuilder(name)(session => Try(f(session)).success)
-  }
-
-  private[this] val scn =
-    scenario("BasicSimulation").exec(grpc("/ping.PingService/Ping") { _ =>
-      protocol.stub.send(Message())
-    })
-
-  before {
-    println(s"start gRPC server $port")
-    server.start()
-  }
-
-  setUp(scn.inject(rampUsersPerSec(100).to(1000).during(10.seconds)))
-
-  after {
-    println("shutdown now")
-    server.shutdownNow()
-    server.awaitTermination()
   }
 }
---
+++ src/test/scala/grpcgatling/ErrorSimulation.scala
@@ -0,0 +1,37 @@
+package grpcgatling
+
+import grpcgatling.grpc.GrpcProtocol
+import grpcgatling.server.{PingServer, UnimplementedPingServer}
+import io.gatling.core.Predef._
+import ping.Message
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
+class ErrorSimulation extends BasicSimulation {
+  private[this] val port = 9001
+
+  private[this] val server = PingServer.server(port,
+                                               ExecutionContext.global,
+                                               new UnimplementedPingServer)
+
+  private[this] val protocol: GrpcProtocol = GrpcProtocol("localhost", port)
+
+  private[this] val scn =
+    scenario("BasicSimulation").exec(grpc("/ping.PingService/Ping") { _ =>
+      protocol.stub.send(Message())
+    })
+
+  before {
+    println(s"start gRPC server $port")
+    server.start()
+  }
+
+  setUp(scn.inject(rampUsersPerSec(100).to(1000).during(10.seconds)))
+
+  after {
+    println("shutdown now")
+    server.shutdownNow()
+    server.awaitTermination()
+  }
+}
